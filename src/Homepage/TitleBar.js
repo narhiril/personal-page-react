19800c13f44823bbbead34181a9e30577ef1d29c
narhiril <roxannarusbarsky@yahoo.com>
2023-03-06 17:50:00 -0600

    work on button icons

--- src/Homepage/TitleBar.js
+++ src/Homepage/TitleBar.js
@@ -1,5 +1,7 @@
 import "./TitleBar.scss";
 import placeholder from "../Homepage/pfp.jpg";
+import linkedinlogo from "../Homepage/LI-Logo.png";
+import githublogo from "../Homepage/github.png";
 
 const TitleBar = () => {
     const email = "roxannarusbarsky@yahoo.com";
@@ -11,9 +13,20 @@ const TitleBar = () => {
             <h2 className="text-sm">Full Stack Web Developer</h2>
           </div>
           <div id="contact-links" className="container text-center">
-                <a className="item-a btn btn-lg btn-outline-dark" title={email} href={`mailto:${email}`}>Email</a>
-                <a className="item-b btn btn-lg btn-outline-dark" title="linkedin.com/in/roxanna-rusbarsky" href="https://www.linkedin.com/in/roxanna-rusbarsky/">LinkedIn</a>
-                <a className="item-c btn btn-lg btn-outline-dark" title="github.com/narhiril" href="https://github.com/narhiril">GitHub</a>
+                <a className="item-a btn btn-lg btn-outline-dark" 
+                   title={email} 
+                   href={`mailto:${email}`}>Email
+                </a>
+                <a className="item-b btn btn-lg btn-outline-dark" 
+                   title="linkedin.com/in/roxanna-rusbarsky" 
+                   href="https://www.linkedin.com/in/roxanna-rusbarsky/">
+                  <img id="linkedin" src={linkedinlogo} alt="LinkedIn" />
+                </a>
+                <a className="item-c btn btn-lg btn-outline-dark" 
+                   title="github.com/narhiril" 
+                   href="https://github.com/narhiril">
+                   <img id="github" src={githublogo} alt="GitHub" />
+                </a>
           </div>
         </div>
     );
